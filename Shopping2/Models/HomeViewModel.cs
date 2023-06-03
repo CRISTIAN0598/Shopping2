297ebbb8363246d9394db7086dc174d97a8958db
Cristian <kriztian31marulanda@gmail.com>
2023-06-03 10:11:11 -0500

    Images zoom & Improve home

--- Shopping2/Models/HomeViewModel.cs
+++ Shopping2/Models/HomeViewModel.cs
@@ -1,8 +1,10 @@
-﻿namespace Shopping2.Models
+﻿using Shopping2.Data.Entities;
+
+namespace Shopping2.Models
 {
     public class HomeViewModel
     {
-        public ICollection<ProductHomeViewModel> Products { get; set; }
+        public ICollection<Product> Products { get; set; }
 
         public float Quantity { get; set; }
 
