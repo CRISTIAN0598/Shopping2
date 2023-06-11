a4281116885fe7589d5b63fff1fdde438eb9a984
Cristian <kriztian31marulanda@gmail.com>
2023-06-11 18:35:45 -0500

    Order,filter,pagination in home and add privacity policy

--- Shopping2/Models/HomeViewModel.cs
+++ Shopping2/Models/HomeViewModel.cs
@@ -1,11 +1,12 @@
-﻿using Shopping2.Data.Entities;
+﻿using Shopping2.Common;
+using Shopping2.Data.Entities;
 
 namespace Shopping2.Models
 {
     public class HomeViewModel
     {
-        public ICollection<Product> Products { get; set; }
-
+        public PaginatedList<Product> Products { get; set; }
+        public ICollection<Category> Categories { get; set; }
         public float Quantity { get; set; }
 
     }
